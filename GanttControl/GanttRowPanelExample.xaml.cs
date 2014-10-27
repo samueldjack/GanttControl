5ed250d8e42cc8f7c339a2c8c0a2561242f1ffd5
Samuel Jack <sam@seaturtlesoftware.com>
2014-10-27 17:28:29 +0000

    Initial commit

---
+++ GanttControl/GanttRowPanelExample.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FunctionalFun.UI
+{
+    /// <summary>
+    /// Interaction logic for GanttRowPanelExample.xaml
+    /// </summary>
+    public partial class GanttRowPanelExample : Window
+    {
+        public GanttRowPanelExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GanttControl/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FunctionalFun.UI
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
